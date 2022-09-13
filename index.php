ac4551ac49db3ecaf082e08a955f2b3171a1a508
vico65 <alvicojr65@gmail.com>
2022-09-13 19:49:08 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,5 +10,6 @@
     <h1>helloworld</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, voluptas praesentium. Ex animi error necessitatibus harum laudantium incidunt, unde nihil fuga qui eligendi obcaecati aspernatur iste dolor facilis, sed asperiores.
     </p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus reiciendis error nemo sequi corrupti facilis debitis at repellat numquam rem officia laudantium, laboriosam expedita quasi corporis cumque quas commodi temporibus minima deleniti ipsum quidem architecto qui illo! Labore at cumque cupiditate maxime illo, officia ad. Exercitationem, mollitia rem perferendis reprehenderit asperiores ducimus molestias tempore in dolores obcaecati similique. Optio, enim!</p>
 </body>
 </html>
